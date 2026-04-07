8951e5463fe912bcd54bc9a7807279ce91a4e6a6
origin <dubnitskaya.katya@mail.ru>
2026-04-07 15:08:16 +0400

    Revert "New"
    This reverts commit 0100b75e6ee859ead2d621dee57e99fd21cc0cf1.

--- src/main/java/com/example/exam/service/ExaminerService.java
+++
@@ -1,7 +0,0 @@
-package com.example.exam.service;
-import com.example.exam.model.Question;
-import java.util.Collection;
-
-public interface ExaminerService {
-    Collection<Question> getQuestions(int amount);
-}
